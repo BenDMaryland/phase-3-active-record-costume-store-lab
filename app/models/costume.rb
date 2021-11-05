4b697ddd78233337e77e6b9ae0f6e181c5ff1d0e
ben.d.maryland@gmail.com <bend@RoseFan.localdomain>
2021-11-05 11:18:30 -0400

    commit tree 9effdaf550e199bcfcaaf4ff5637650ae2718cc9

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,8 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
+
+class Costume <ActiveRecord:: Migration[6.1]
+
+
+
+end
--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,6 @@
 # Create your CostumeStore class here
+class CostumeStore <ActiveRecord:: Migration[6.1]
+
+
+
+end
